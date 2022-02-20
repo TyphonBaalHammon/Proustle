7c310093e296d0807491d58f153b174fff6c896e
TyphonBaalHammon <typhon@baalhammon.fr>
2022-02-21 00:31:28 +0100

    rajout fichiers

---
+++ js/dates.js
@@ -0,0 +1,16 @@
+(function (factory) {
+    if (typeof module === "object" && typeof module.exports === "object") {
+        var v = factory(require, exports);
+        if (v !== undefined) module.exports = v;
+    }
+    else if (typeof define === "function" && define.amd) {
+        define(["require", "exports"], factory);
+    }
+})(function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    exports.dateOrigine = void 0;
+    var dateOrigine = new Date(2022, 1, 20);
+    exports.dateOrigine = dateOrigine;
+});
+//# sourceMappingURL=dates.js.map
